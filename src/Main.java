56f5b8a4ec0833d6773b3f942b42710eb9c91b82
Regina <regina.vagapova95@gmail.com>
2023-10-31 12:36:31 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println("результат умножения 11 и 101 = " + binOps.mult("11", "101"));
+        System.out.println("результат сложения 11 и 101 = " + binOps.sum("11", "101"));
+    }
+}
